f8008b6fdebbba3f550e618b7321b3fc9470ef56
almaayn20 <68010826+almaayn20@users.noreply.github.com>
2020-07-08 16:15:00 +0300

    Update and rename Updater.json to Updater.js

--- Updater.js
+++ Updater.js
@@ -1,7 +1,7 @@
 {
   "records": [
     {
-      "version": "DELTA YOWA v3.3.2",
+      "version": "DELTA YOWA v4.3.2",
       "code": 11,
       "url": "https://bit.ly/NUSANTARAPROJECT",
       "changelog": " - Auuto Forward Text Message - Schedule Message - Auto Reply Message - Movable Floating Chat View - Fix Crash in Toast Statuses - Fix Crash old Stock Entry Style - Fix Home Row Height - Show Hide Fast Scroll - Any more"
